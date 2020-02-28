8aee6083360c3e8ee5791f8b7b2c58707652507d
Poojajmpatil <41111770+Poojajmpatil@users.noreply.github.com>
2020-02-28 01:54:16 -0800

    Add files via upload

---
+++ stage01-adb-group-service/target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO groups
+VALUES('FAMILY','FAMILY MEMBERS','All my family members');
+INSERT INTO groups
+VALUES('WORK','My Work Mates','All my work mates');
+INSERT INTO groups
+VALUES('Gym','Gym Friends','All my Gym Friends');
